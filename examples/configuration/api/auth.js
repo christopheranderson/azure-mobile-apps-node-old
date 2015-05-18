a67fc0795b32d1cc06b0d157a10a7619bafec67f
Dale Anderson <dalander@microsoft.com>
2015-05-18 11:46:06 -0700

    Added res.status

--- examples/configuration/api/auth.js
+++ examples/configuration/api/auth.js
@@ -3,7 +3,7 @@
     get: function (req, res, next) {
         // the auth middleware must have been executed for the user property to be set
         if(req.user.level === 'anonymous')
-            res.send("We don't know you. Who are you?");
+            res.status(401).send("We don't know you. Who are you?");
         else
             // the user object contains the user ID and payload
             res.send("Hello " + req.user.id + "! Your payload is " + JSON.stringify(req.user.payload));
