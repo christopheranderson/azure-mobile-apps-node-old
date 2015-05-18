6a7338ee5367dfe9ab64340e815e14f49121aa6f
Dale Anderson <dalander@microsoft.com>
2015-05-18 13:11:41 -0700

    Added error handler

--- examples/middleware.js
+++ examples/middleware.js
@@ -23,6 +23,17 @@ app.get('/test2', function (req, res, next) {
     res.end('You hit the test2 endpoint!');
 });
 
+app.get('/error', function (req, res, next) {
+    next(new Error("Something went wrong"));
+});
+
+app.use(function (err, req, res, next) {
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack
+    })
+});
+
 app.use(function (req, res) {
     res.status(404).end('Not found');
 });
