498a9d144c749cd1f195f9b8df84e377d6d6353b
Dale Anderson <dalander@microsoft.com>
2015-05-18 13:41:47 -0700

    Added extra auth scenario

--- examples/configuration/api/auth.js
+++ examples/configuration/api/auth.js
@@ -7,5 +7,10 @@
         else
             // the user object contains the user ID and payload
             res.send("Hello " + req.user.id + "! Your payload is " + JSON.stringify(req.user.payload));
+    },
+    post: function (req, res, next) {
+        return req.user.getProviders().then(function (providers) {
+            res.send("Your providers are " + JSON.stringify(providers));
+        })
     }
 }
