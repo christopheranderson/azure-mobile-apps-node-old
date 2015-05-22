4a14db193d5df7f3cf348c98d1661564ff61bae2
Dale Anderson <dalander@microsoft.com>
2015-05-22 10:08:01 -0700

    Removed prototype

--- examples/minimal.js
+++
@@ -1,7 +0,0 @@
-﻿var app = require('express')(),
-    zumo = require('zumo')();
-
-// table configuration is imported from /tables by default
-zumo.attach(app);
-
-app.listen(1337);
--- examples/quickstart.js
+++
@@ -1,7 +0,0 @@
-﻿var app = require('express')(),
-    zumo = require('zumo')();
-
-zumo.tables.add('todoitem');
-zumo.attach(app);
-app.listen(process.env.PORT);
-    
