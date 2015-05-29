3a3c73adc48120b76c42253d44ccfbe3c3600719
Dale Anderson <dalander@microsoft.com>
2015-05-29 11:52:38 -0700

    Added integration tests

---
+++ samples/tables/tableController.js
@@ -0,0 +1,17 @@
+﻿var todoitem = require('azure-mobile-apps').table();
+
+todoitem.read(function (query, context) {
+
+});
+
+todoitem.update(function (item, context) {
+
+});
+
+todoitem.insert(function (item, context) {
+
+});
+
+todoitem.delete(function (id, context) {
+
+});
