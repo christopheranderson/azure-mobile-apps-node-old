6c9fa3b95faefb58aadfc9728f03f58365fc4b23
Dale Anderson <dalander@microsoft.com>
2015-07-22 16:02:55 -0700

    Fixed result behavior

--- test/E2E/index.js
+++ test/E2E/index.js
@@ -17,14 +17,13 @@ mobileApp.tables.add('blog_comments', { columns: { postId: 'string', commentText
 mobileApp.tables.add('blog_posts', { columns: { title: 'string', commentCount: 'number', showComments: 'boolean', data: 'string' } });
 mobileApp.tables.add('dates', { columns: { date: 'date', dateOffset: 'date' } });
 mobileApp.tables.add('movies', { columns: { title: 'string', duration: 'number', mpaaRating: 'string', releaseDate: 'date', bestPictureWinner: 'boolean' } });
-mobileApp.tables.add('ParamsTestTable');
 mobileApp.tables.add('IntIdRoundTripTable', { autoIncrement: true, columns: { name: 'string', date1: 'date', bool: 'boolean', integer: 'number', number: 'number' } });
 mobileApp.tables.add('intIdMovies', { autoIncrement: true, columns: { title: 'string', duration: 'number', mpaaRating: 'string', releaseDate: 'date', bestPictureWinner: 'boolean' } });
 mobileApp.tables.add('OfflineReady');
 mobileApp.tables.add('OfflineReadyNoVersionAuthenticated', { authorise: true });
 
-var table = mobileApp.table();
-table.update(function (context) {
+var roundTrip = mobileApp.table();
+roundTrip.update(function (context) {
     return context.execute()
         .catch(function (error) {
             if(context.req.query.conflictPolicy === 'clientWins') {
@@ -37,8 +36,28 @@ table.update(function (context) {
             }
         });
 });
-table.columns = { name: 'string', date1: 'date', bool: 'boolean', integer: 'number', number: 'number' };
-mobileApp.tables.add('roundTripTable', table);
+roundTrip.columns = { name: 'string', date1: 'date', bool: 'boolean', integer: 'number', number: 'number' };
+mobileApp.tables.add('roundTripTable', roundTrip);
+
+var parameterTest = mobileApp.table();
+parameterTest.autoIncrement = true;
+parameterTest.read(function (context) {
+    // read operations expect to return an array... this could be improved
+    return [mapParameters(context)];
+});
+parameterTest.insert(mapParameters);
+parameterTest.update(mapParameters);
+parameterTest.delete(mapParameters);
+mobileApp.tables.add('ParamsTestTable', parameterTest);
+
+function mapParameters(context) {
+    var id = parseInt(context.id || context.req.query.id || '1');
+    return {
+        id: id,
+        parameters: JSON.stringify(context.req.query)
+    };
+}
+
 
 mobileApp.attach(app);
 
