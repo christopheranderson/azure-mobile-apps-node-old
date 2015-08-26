11d0a39ab097507922ba49a8541721e5a10ace1c
Dale Anderson <danderson00@gmail.com>
2015-08-25 19:44:18 -0700

    Auth and formatting updates

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,32 +1,51 @@
+/**
+@module azure-mobile-apps/auth
+@description Helper functions for working with JWT tokens
+*/
 var user = require('./user'),
     jwt = require('jsonwebtoken'),
     crypto = require('crypto'),
     promises = require('../utilities/promises');
 
-module.exports = function (config) {
-    var key = hashSecret(config.secret);
+/**
+Create an instance of a helper based on the supplied configuration.
+@param {authConfiguration} configuration The authentication configuration
+@returns An object with members described below.
+*/
+module.exports = function (configuration) {
+    var key = hashSecret(configuration.secret);
 
     return {
+        /**
+        Validate a JWT token
+        @param {string|Buffer} token The JWT token to validate
+        @returns A promise that yields a {@link module:azure-mobile-apps/auth/user user} object on success.
+        */
         validate: function (token) {
             return promises.create(function (resolve, reject) {
                 var options = {
-                    audience: config.audience || 'urn:microsoft:windows-azure:zumo',
-                    issuer: config.issuer || 'urn:microsoft:windows-azure:zumo'
+                    audience: configuration.audience || 'urn:microsoft:windows-azure:zumo',
+                    issuer: configuration.issuer || 'urn:microsoft:windows-azure:zumo'
                 };
 
                 jwt.verify(token, key, options, function (err, claims) {
                     if(err)
                         reject(err);
                     else
-                        resolve(user(config, token, claims));
+                        resolve(user(configuration, token, claims));
                 });
             });
         },
+        /**
+        Create a token from the specified payload
+        @param {object} payload The payload to sign
+        @returns {string} The signed token.
+        */
         sign: function (payload) {
             var options = {
-                audience: config.audience || 'urn:microsoft:windows-azure:zumo',
-                issuer: config.issuer || 'urn:microsoft:windows-azure:zumo',
-                expiresInMinutes: config.expires || 1440
+                audience: configuration.audience || 'urn:microsoft:windows-azure:zumo',
+                issuer: configuration.issuer || 'urn:microsoft:windows-azure:zumo',
+                expiresInMinutes: configuration.expires || 1440
             };
             return jwt.sign(payload, key, options);
         }
